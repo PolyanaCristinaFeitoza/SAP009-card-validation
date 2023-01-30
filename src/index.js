81b20a474d34453dbd3438c9142d7493a0ff0566
Polyana <polyanacristina2001@gmail.com>
2023-01-30 20:14:54 -0300

    guardar valor digitado no input invisível

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ alert(`Olá, ${nome}!`)
 
 document.querySelector('#botao').onclick = () => {
   const divResultado = document.querySelector('#resultado')
-  const valor = document.querySelector('#input').value
+  const valor = document.querySelector('#inputEscondido').value
   //se o valor digitado for menor que 1, irá retornar um alert
   if (valor.length < 1) {
     alert('Digite um número válido')
@@ -17,8 +17,16 @@ document.querySelector('#botao').onclick = () => {
     divResultado.innerHTML = 'Número inválido, digite novamente.'
   }
 }
+document.querySelector('#input').oninput = (value) => {
+  const inputEscondido = document.querySelector('#inputEscondido')
+  const valorInputEscondido = inputEscondido.value
 
-document.querySelector('#input').oninput = () => {
+  if (value.data === null && valorInputEscondido.length > 0) {
+    inputEscondido.value = inputEscondido.value.slice(0, -1)
+  } else {
+    inputEscondido.value += value.data
+  }
+
   const valor = document.querySelector('#input').value
   const valorFormatado = validator.maskify(valor)
   document.querySelector('#input').value = valorFormatado
