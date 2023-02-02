a6a4b00165de740a0698d503a90241204a85fcf0
Polyana <polyanacristina2001@gmail.com>
2023-02-02 15:36:25 -0300

    Updates

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ alert(`Olá, ${nome}!`)
 
 document.querySelector('#botao').onclick = () => {
   const divResultado = document.querySelector('#resultado')
-  const valor = document.querySelector('#input').value
+  const valor2 = document.querySelector('#inputEscondido').value
   //se o valor digitado for menor que 1, irá retornar um alert
-  if (valor.length < 1) {
+  if (valor2.length < 1) {
     alert('Digite um número válido')
     return
   }
-  const numeroValido = validator.isValid(valor)
+  const numeroValido = validator.isValid(valor2)
   if (numeroValido === true) {
     divResultado.innerHTML = 'Compra realizada com sucesso.'
   } else {
     divResultado.innerHTML = 'Número inválido, digite novamente.'
   }
 }
+document.querySelector('#input').oninput = (value) => {
+  const inputEscondido = document.querySelector('#inputEscondido')
+  const valorInputEscondido = inputEscondido.value
 
-document.querySelector('#input').oninput = () => {
+  if (value.data === null && valorInputEscondido.length > 0) {
+    inputEscondido.value = inputEscondido.value.slice(0, -1)
+  } else {
+    inputEscondido.value += value.data
+  }
+
   const valor = document.querySelector('#input').value
   const valorFormatado = validator.maskify(valor)
   document.querySelector('#input').value = valorFormatado
